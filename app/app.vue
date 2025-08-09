0c4360c199ec34e632f432c7b76ed7ea499c34f3
John William Davis <jwdavisdeveloper@gmail.com>
2025-08-09 13:18:23 -0700

    add image and config to nuxt config

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,11 @@
 <template>
     <main>
         <h1 class="text-red-600 text-2xl font-serif">vertical experiments</h1>
+        <NuxtImg
+            src="/images/bridge.jpg"
+            height="600"
+            width="640"
+            alt="Bridge in a forest"
+        ></NuxtImg>
     </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,30 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false, // Remove path prefix from component names
+    },
+  ],
+  image: {
+    provider: "vercel",
+    quality: 80,
+    format: ["webp", "jpeg"],
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      xxxl: 2048,
+    },
+    densities: [1, 2],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
+
+  modules: ["@nuxt/image"],
 });
